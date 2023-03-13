db385bb6d5b1804e774f15517521426dd13f62e8
hanbinChae <hanbin2043@gmail.com>
2023-03-13 16:09:37 +0900

    js basic

--- prac/example04.js
+++ prac/example04.js
@@ -3,3 +3,17 @@ let array = [1,2,3,'test','test2',1,2,3,4]
 console.log(array)
 console.log(array[0])
 console.log(array[3])
+
+for(let i=0; i<array.length; i++){
+    console.log(array[i])
+}
+
+console.log("----------for of--------------")
+for(el of array){
+    console.log(el)
+}
+
+console.log("---------map---------------")
+array.map((data) => {
+    console.log(data)
+});
