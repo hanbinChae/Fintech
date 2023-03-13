a9655fe91be9a0106b75dcb5bece52af128f3c4f
hanbinChae <hanbin2043@gmail.com>
2023-03-13 16:26:01 +0900

    구조 분해 할당 구문, 템플릿리터럴

---
+++ prac/example06.js
@@ -0,0 +1,10 @@
+let url = 'https://naver.com'
+
+console.log(url);
+console.log(url+"/news")
+console.log(url+"/news"+"/test");
+
+let userName = "채한빈";
+console.log(`안녕하세요 ${userName}님! 반갑습니다.`)
+console.log(`${url}/news/test`);
+
