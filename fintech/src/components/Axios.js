0c6880b1927e4924c450aa76af35047296410b42
hanbinChae <hanbin2043@gmail.com>
2023-03-14 16:44:22 +0900

    using newsAPI, get contents

--- fintech/src/components/Axios.js
+++ fintech/src/components/Axios.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from "axios"
 
 function Axios() {
+    const [news,setNews] = useState();
     const handleClick = () =>{
-        axios.get("https://naver.com").then((response)=>{
+        axios.get("https://newsapi.org/v2/everything?q=tesla&from=2023-02-14&sortBy=publishedAt&apiKey=c7d69e0ea54e4c4e913573095d8afef4").then((response)=>{
             console.log(response.data);
-        })
-    }
+            setNews(response.data)
+        });
+    };
 
   return (
     <div>
+        {news}
         <button onClick={handleClick}>통신 발생</button>
     </div>
   )
