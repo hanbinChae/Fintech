520cab8364d1212bc726328f3700545195cddff2
hanbinChae <hanbin2043@gmail.com>
2023-03-14 14:30:22 +0900

    react Event, State

--- fintech/src/components/State.js
+++ fintech/src/components/State.js
@@ -1,6 +1,23 @@
-const State = () =>{
-    return <p>스테이트</p>;
+import { useState } from "react";
 
-}
+const State = () => {
+  let [inputText, setInputText] = useState();
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    setInputText(value);
+  };
+
+  const handleClick = () => {
+    console.log("클릭!");
+  };
+
+  return (
+    <div>
+      <p>{inputText}</p>
+      <input onChange={handleChange}></input>
+      <button onClick={handleClick}>출력</button>
+    </div>
+  );
+};
 
 export default State;
