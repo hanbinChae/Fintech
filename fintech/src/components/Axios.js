b401dcba497390e3f75c5b71012e00d5af19bcb5
hanbinChae <hanbin2043@gmail.com>
2023-03-14 17:10:34 +0900

    query string + news API 를 통해 원하는 기사 요청 및 응답 반환

--- fintech/src/components/Axios.js
+++ fintech/src/components/Axios.js
@@ -2,17 +2,26 @@ import React, {useState} from 'react'
 import axios from "axios"
 
 function Axios() {
+    const [searchInput, setSearchInput] = useState("");
     const [news,setNews] = useState();
+
     const handleClick = () =>{
-        axios.get("https://newsapi.org/v2/everything?q=tesla&from=2023-02-14&sortBy=publishedAt&apiKey=c7d69e0ea54e4c4e913573095d8afef4").then((response)=>{
+        let url = `https://newsapi.org/v2/everything?q=${searchInput}&from=2023-02-14&sortBy=publishedAt&apiKey=c7d69e0ea54e4c4e913573095d8afef4`
+
+        axios.get(url).then((response)=>{
             console.log(response.data);
             setNews(response.data)
         });
     };
 
+    function handleChange({target}){
+        const {value} = target;
+        setSearchInput(value);
+    }
+    
   return (
     <div>
-        {news}
+        <input onChange={handleChange}></input>
         <button onClick={handleClick}>통신 발생</button>
     </div>
   )
