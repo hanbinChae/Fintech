6106a9f57b5a503406b850e76a2d688a46e245d3
hanbinChae <hanbin2043@gmail.com>
2023-03-14 14:47:23 +0900

    List components

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import Welcome from "./components/Welcome";
 import State from "./components/State";
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
       <Welcome userName="채한빈"></Welcome>
       <Welcome userName="홍길동"></Welcome>
-      <Welcome userName="길딩동"></Welcome>
-      <Welcome userName="도레미"></Welcome>
       
       <State></State>
-
+      <List></List>
     </div>
   );
 }
---
+++ fintech/src/components/List.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const List = () => {
+  const [users, setUsers] = useState([
+    { name: "홍길동", age: 12, height: 180 },
+    { name: "임창정", age: 18, height: 180 },
+    { name: "채한빈", age: 16, height: 180 },
+  ]);
+
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <>
+            <p>{user.name}님</p>
+            <p>{user.age}세</p>
+            <p>{user.height}cm</p>
+          </>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
