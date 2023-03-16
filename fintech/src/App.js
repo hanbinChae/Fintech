d895e7827c903168ba441593116cf928a9169ef6
hanbinChae <hanbin2043@gmail.com>
2023-03-16 16:11:34 +0900

    잔액조회 기능 작성

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -6,6 +6,7 @@ import NewsSearch from "./pages/NewsSearch";
 import Auth from "./pages/Auth";
 import AuthResult from "./pages/AuthResult";
 import Account from "./pages/Account";
+import Balance from "./pages/Balance";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/auth" element={<Auth></Auth>}></Route>
           <Route path="/authResult" element={<AuthResult></AuthResult>}></Route>
           <Route path="/main" element={<Account></Account>}></Route>
+          <Route path="/balance" element={<Balance></Balance>}></Route>
         
         </Routes>
       </BrowserRouter>
---
+++ fintech/src/pages/Balance.js
@@ -0,0 +1,58 @@
+import queryString from "query-string";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import AppHeader from "../components/common/AppHeader";
+import axios from "axios";
+const Balance = () => {
+  const [balance, setBalance] = useState(0);
+  const query = useLocation().search;
+  const fintechUseNo = queryString.parse(query).fintechUseNo;
+
+  useEffect(() => {
+    getBalance();
+  }, []);
+
+  const genTransId = () => {
+    let countnum = Math.floor(Math.random() * 1000000000) + 1;
+    const clientNo = "M202300621";
+    let transId = clientNo + "U" + countnum;
+    return transId;
+  };
+  console.log(fintechUseNo);
+
+  const getBalance = () => {
+    console.log(genTransId());
+    const accessToken = localStorage.getItem("accessToken");
+
+    const sendData = {
+      bank_tran_id: genTransId(),
+      fintech_use_num: fintechUseNo,
+      tran_dtime: "20230316141800",
+    };
+
+    const option = {
+      method: "GET",
+      url: "v2.0/account/balance/fin_num",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: sendData,
+    };
+
+    axios(option).then(({ data }) => {
+      console.log(data);
+      setBalance(data.balance_amt);
+      //   setBalance(data.res_list);
+    });
+  };
+
+  return (
+    <div>
+      <AppHeader title={"잔액조회"}></AppHeader>
+      귀하의 잔액은 :{balance} 입니다.
+    </div>
+  );
+};
+
+export default Balance;
