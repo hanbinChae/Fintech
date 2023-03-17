e4dc603f750a8bcb701384df2f6c757e29d90ffe
hanbinChae <hanbin2043@gmail.com>
2023-03-17 16:06:52 +0900

    출금, 입금

--- fintech/src/components/withdraw/ModalCard.js
+++ fintech/src/components/withdraw/ModalCard.js
@@ -39,21 +39,87 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
   };
 
   const handlePayButtonClick = () => {
-    // 출금 이체 발생시키기
-    // data params json
-    // tran_amt, fintech_use_num, req_client_fintech_use_num, bank_tran_id 고정값 사용 금지 나머지는 고정값으로
-    // axios option으로 요청을 작성하기 <- api 요청
-    // application/json 은 데이터를 어떻게 전송?
-    // 결과를 로그로 작성
+    const accessToken = localStorage.getItem("accessToken");
+
+    const data = {
+      bank_tran_id: genTransId(),
+      cntr_account_type: "N",
+      cntr_account_num: "100000000001",
+      dps_print_content: "쇼핑몰환불",
+      fintech_use_num: fintechUseNo,
+      wd_print_content: "오픈뱅킹출금",
+      tran_amt: amount,
+      tran_dtime: "20230317130000",
+      req_client_name: "홍길동",
+      req_client_fintech_use_num: fintechUseNo,
+      req_client_num: "HONGGILDONG1234",
+      transfer_purpose: "ST",
+      recv_client_name: "채한빈",
+      recv_client_bank_code: "097",
+      recv_client_account_num: "100000000001",
+    };
+
+    const option = {
+      method: "POST",
+      url: "/v2.0/transfer/withdraw/fin_num",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: data,
+    };
+
+    axios(option).then(({ data }) => {
+      if (data.rsp_code === "A0000") {
+        alert("출금 이체가 성공했습니다!");
+        deposit();
+      } else {
+        alert("출금 이체가 실패하였습니다!");
+      }
+    });
   };
 
   const deposit = () => {
-    /**
-     * #Last Work
-     * 입금이체 작성해 주세요 !
-     * 2legged token 사용 !
-     * 입금을 하는 계좌를 잘 선택해 주세요
-     */
+
+    const twoLeggedToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJNMjAyMzAwNDQwIiwic2NvcGUiOlsib29iIl0sImlzcyI6Imh0dHBzOi8vd3d3Lm9wZW5iYW5raW5nLm9yLmtyIiwiZXhwIjoxNjg2ODEwOTY0LCJqdGkiOiI3ZTc4NWU3ZC1iNTQyLTQ4ZGItYmNjOS1mNzczZTY1NzNkMzYifQ.GE163H544kkj4snzr1jvMm69-ra1ra-MPvKgxfJNk_I";
+    //2legged Token 입력바랍니다.
+
+    const data = {
+      cntr_account_type: "N",
+      cntr_account_num: "200000000001",
+      wd_pass_phrase: "NONE",
+      wd_print_content: "환불금액",
+      name_check_option: "off",
+      tran_dtime: "20220812130000",
+      req_cnt: "1",
+      req_list: [
+        {
+          tran_no: "1",
+          bank_tran_id: genTransId(),
+          fintech_use_num: tofintechno,
+          print_content: "오픈서비스캐시백",
+          tran_amt: amount,
+          req_client_name: "채한빈",
+          req_client_fintech_use_num: fintechUseNo,
+          req_client_num: "SKYMILL2000",
+          transfer_purpose: "ST",
+        },
+      ],
+    };
+    const option = {
+      method: "POST",
+      url: "/v2.0/transfer/deposit/fin_num",
+      headers: {
+        Authorization: `Bearer ${twoLeggedToken}`,
+      },
+      data: data,
+    };
+
+    axios(option).then(({ data }) => {
+      if (data.rsp_code === "A0000") {
+        alert("결제 완료 !");
+      }
+    });
   };
 
   const handleChange = (e) => {
